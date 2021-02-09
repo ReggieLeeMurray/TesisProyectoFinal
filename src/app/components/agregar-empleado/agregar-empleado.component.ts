6574385ad9e13f3ef53ca160bf1bbf868803a1e7
ReggieLeeMurray <ReggieLeeMurray@users.noreply.github.com>
2021-02-09 13:21:10 -0600

    creacion y estructura inicial del backend

--- src/app/components/agregar-empleado/agregar-empleado.component.ts
+++ src/app/components/agregar-empleado/agregar-empleado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup , FormBuilder, Validators } from "@angular/forms";
 import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-empleado',
@@ -9,23 +10,43 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class AgregarEmpleadoComponent implements OnInit {
   empleadosForm: FormGroup;
-
-  constructor(private fb:FormBuilder) {
+  idEmpleado=0;
+  accion='Agregar';
+  constructor(private fb:FormBuilder, private route: ActivatedRoute) {
     this.empleadosForm=this.fb.group({
-      id:['', Validators.required],
+      //id:['', Validators.required],
       nombre:['', Validators.required],
       apellido:['', Validators.required],
       n_cedula:['', Validators.required],
-      fechaingreso:['', Validators.required],
+      //fechaingreso:['', Validators.required],
       direccion:['', Validators.required],
       depto:['', Validators.required],
       salariobase:['', Validators.required],
-    })
+    });
+    if (+this.route.snapshot.paramMap.get('id')>0){
+      this.idEmpleado=+this.route.snapshot.paramMap.get('id')
+    }
    }
 
   ngOnInit(): void {
+    this.esEditar();
   }
   guardarEmpleados(){
   console.log(this.empleadosForm)
+  }
+  esEditar(){
+    if(this.idEmpleado>0){
+      this.accion='Editar';
+      this.empleadosForm.patchValue({
+        id: '1',
+        nombre: 'Humberto',
+        apellido: 'Zen',
+        n_cedula: '0512-1972-01540',
+        fechaingreso: '11/12/2011',
+        depto: 'Contabilidad',
+        salariobase: '20000',
+      })
+
+    }
   }
 }
