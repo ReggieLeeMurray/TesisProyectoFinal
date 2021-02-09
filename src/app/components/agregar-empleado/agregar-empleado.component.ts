188a935a30100625c0671092616d1c27e4278c81
ReggieLeeMurray <ReggieLeeMurray@users.noreply.github.com>
2021-02-09 10:51:53 -0600

    form nuevo empleado

--- src/app/components/agregar-empleado/agregar-empleado.component.ts
+++ src/app/components/agregar-empleado/agregar-empleado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder, Validators } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-empleado',
@@ -6,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-empleado.component.css']
 })
 export class AgregarEmpleadoComponent implements OnInit {
+  empleadosForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {
+    this.empleadosForm=this.fb.group({
+      id:['', Validators.required],
+      nombre:['', Validators.required],
+      apellido:['', Validators.required],
+      n_cedula:['', Validators.required],
+      fechaingreso:['', Validators.required],
+      depto:['', Validators.required],
+      salariobase:['', Validators.required],
+    })
+   }
 
   ngOnInit(): void {
   }
--- src/app/models/empleado.ts
+++ src/app/models/empleado.ts
@@ -1,9 +1,10 @@
 export class Empleado {
     id?: number;
-    nombre!: string;
-    apellido!: string;
-    n_cedula!: number;
-    fechaingreso!: Date;
-    depto!: string;
-    salariobase!: number;
+    nombre: string;
+    apellido: string;
+    n_cedula: number;
+    direccion: string;
+    fechaingreso: Date;
+    depto: string;
+    salariobase: number;
 }
